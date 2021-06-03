6992fece34d4fceb1ef8797396fdc14c6dc4e0c0
snamiki1212 <snamiki1212@gmail.com>
2021-06-03 16:40:28 -0700

    Add user-detail template

--- Habits/Habits/Controllers/User/UserCollectionViewController.swift
+++ Habits/Habits/Controllers/User/UserCollectionViewController.swift
@@ -97,34 +97,11 @@ class UserCollectionViewController: UICollectionViewController {
 
     // MARK: UICollectionViewDelegate
 
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        guard let item = dataSource.itemIdentifier(for: indexPath) else { return }
+        let vc = UserDetailViewController(user: item.user)
+        navigationController?.pushViewController(vc, animated: true)
     }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
-    
-    }
-    */
 
     
     func update() {
--- Habits/Habits/Controllers/User/UserDetailViewController.swift
+++ Habits/Habits/Controllers/User/UserDetailViewController.swift
@@ -9,9 +9,17 @@ import UIKit
 
 class UserDetailViewController: UIViewController {
 
+    init(user: User) {
+        super.init(nibName: nil, bundle: nil)
+        title = user.name
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        title = "USER_DETAIL" // TODO:
+        view.backgroundColor = .white
     }
 }
